9795ae470fdbbe71dfafe6685ba8b9d697d30524
Allen Mrazek <amrazek@hotmail.com>
2015-01-02 22:13:10 -0800

    First success in loading modded type

---
+++ TestProject/TestReloadProject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace TestProject
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    public class TestReloadProject : MonoBehaviour
+    {
+        void Start()
+        {
+            //gameObject.GetComponents<Component>().ToList().ForEach(c => print("Have component: " + c.name));
+            WriteString("TestReloadProject: " + typeof(TestReloadProject).FullName);
+        }
+
+        void Update()
+        {
+            WriteString("TestReloadProject: " + typeof(TestReloadProject).FullName + " from " + Assembly.GetExecutingAssembly().CodeBase);
+            //var r = new SecondClass("secondclass create"); // to verify namespace change hasn't broken something internally
+        }
+
+        public static void WriteString(string str)
+        {
+            print(str);
+        }
+    }
+}
