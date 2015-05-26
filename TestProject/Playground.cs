452717a07b62677fbd2f1f5ed74f77d5f5387272
Allen Mrazek <amrazek@hotmail.com>
2015-05-25 21:23:12 -0700

    removed extra code from test project

--- TestProject/Playground.cs
+++ TestProject/Playground.cs
@@ -1,9 +1,19 @@
-﻿namespace TestProject
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ReeperCommon.Containers;
+using ReeperCommon.Extensions;
+using ReeperCommon.Logging;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace TestProject
 {
-
-
-
-
+    
+    
 
 
 }
